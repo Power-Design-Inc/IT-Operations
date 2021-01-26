65079928cb1f782d5e1439b895ff6b910a84b9f1
T-Money <50927281+T-money29@users.noreply.github.com>
2021-01-26 08:49:30 -0500

    Nimble Clone Update

--- Oracle/Clone-powershell/nimble-DS-clone.ps1
+++ Oracle/Clone-powershell/nimble-DS-clone.ps1
@@ -1,6 +1,6 @@
-#param (
-#    [string]$labn = $(Read-Host "Oracle Lab Instance Value (1,2,3,4,5)" )
-#)
+param (
+    [string]$labn = $(Read-Host "Oracle Lab Instance Value (01,02,03,04,05)" )
+)
 
 # Login to the Nimble Volume
 # 
@@ -16,8 +16,8 @@ $labdb4 = "LABORADB" + $labn + "4"
 $labdb5 = "LABORADB" + $labn + "5"
 $labdb6 = "LABORADB" + $labn + "6"
 $labdb7 = "LABORADB" + $labn + "7"
-$snapapp = 
-$snapdb = 
+$snapapp = ""
+$snapdb = ""
 
 # Disconnect, disassociate, and remove the Nimble volumes associated with the LAB instance 
 #  
@@ -25,61 +25,76 @@ $response = Get-NSVolume -name $labapp
 $response | ConvertTo-Json
 $responseid = $response.access_control_records.vol_id
 $action = Set-NSVolume -id $responseid -online $false -force $true
-$action = Remove-NSSnapshotCollection -id $responseid
+#$action = Remove-NSSnapshotCollection -id $responseid
 $action = Remove-NSVolume -id $responseid
 
 $response = Get-NSVolume -name $labdb1
 $response | ConvertTo-Json
 $responseid = $response.access_control_records.vol_id
 $action = Set-NSVolume -id $responseid -online $false -force $true
-$action = Remove-NSSnapshotCollection -id $responseid
+#$action = Remove-NSSnapshotCollection -id $responseid
 $action = Remove-NSVolume -id $responseid
 
 $response = Get-NSVolume -name $labdb2
 $response | ConvertTo-Json
 $responseid = $response.access_control_records.vol_id
 $action = Set-NSVolume -id $responseid -online $false -force $true
-$action = Remove-NSSnapshotCollection -id $responseid
+#$action = Remove-NSSnapshotCollection -id $responseid
 $action = Remove-NSVolume -id $responseid
 
 $response = Get-NSVolume -name $labdb3
 $response | ConvertTo-Json
 $responseid = $response.access_control_records.vol_id
 $action = Set-NSVolume -id $responseid -online $false -force $true
-$action = Remove-NSSnapshotCollection -id $responseid
+#$action = Remove-NSSnapshotCollection -id $responseid
 $action = Remove-NSVolume -id $responseid
 
 $response = Get-NSVolume -name $labdb4
 $response | ConvertTo-Json
 $responseid = $response.access_control_records.vol_id
 $action = Set-NSVolume -id $responseid -online $false -force $true
-$action = Remove-NSSnapshotCollection -id $responseid
+#$action = Remove-NSSnapshotCollection -id $responseid
 $action = Remove-NSVolume -id $responseid
 
 $response = Get-NSVolume -name $labdb5
 $response | ConvertTo-Json
 $responseid = $response.access_control_records.vol_id
 $action = Set-NSVolume -id $responseid -online $false -force $true
-$action = Remove-NSSnapshotCollection -id $responseid
+#$action = Remove-NSSnapshotCollection -id $responseid
 $action = Remove-NSVolume -id $responseid
 
 $response = Get-NSVolume -name $labdb6
 $response | ConvertTo-Json
 $responseid = $response.access_control_records.vol_id
 $action = Set-NSVolume -id $responseid -online $false -force $true
-$action = Remove-NSSnapshotCollection -id $responseid
+#$action = Remove-NSSnapshotCollection -id $responseid
 $action = Remove-NSVolume -id $responseid
 
 $response = Get-NSVolume -name $labdb7
 $response | ConvertTo-Json
 $responseid = $response.access_control_records.vol_id
 $action = Set-NSVolume -id $responseid -online $false -force $true
-$action = Remove-NSSnapshotCollection -id $responseid
+#$action = Remove-NSSnapshotCollection -id $responseid
 $action = Remove-NSVolume -id $responseid
 
 # Start the clone process
+# Discover the last snap ID to be taken on the Oracle App volume
 #
-New-NSVolume -name $labapp -base_snap_id $snapapp -clone $true -online $true
+$lastsnap = get-NSvolume -name PDIVLORACLAPP | select last_snap
+$lastsnap | ConvertTo-Json
+$lastsnaptimeid = $lastsnap.last_snap.snap_id
+$lastsnaptimeid
+
+# Create the snap volumes with lab names
+#
+$action = New-NSvolume -name $labapp -clone $true -base_snap_id $lastsnaptimeid
+$action = New-NSvolume -name $labdb1 -clone $true -base_snap_id $lastsnaptimeid
+$action = New-NSvolume -name $labdb2 -clone $true -base_snap_id $lastsnaptimeid
+$action = New-NSvolume -name $labdb3 -clone $true -base_snap_id $lastsnaptimeid
+$action = New-NSvolume -name $labdb4 -clone $true -base_snap_id $lastsnaptimeid
+$action = New-NSvolume -name $labdb5 -clone $true -base_snap_id $lastsnaptimeid
+$action = New-NSvolume -name $labdb6 -clone $true -base_snap_id $lastsnaptimeid
+$action = New-NSvolume -name $labdb7 -clone $true -base_snap_id $lastsnaptimeid
 
 ##Getting initiator group and granting access
 New-NSAccessControlRecord -initiator_group_id #################  -vol_id #########################
