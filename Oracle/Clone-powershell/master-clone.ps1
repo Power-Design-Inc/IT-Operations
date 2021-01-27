6dc0ec644a4cc146e16431fff17432ce4699872b
T-Money <50927281+T-money29@users.noreply.github.com>
2021-01-27 09:26:36 -0500

    Added SDT & Regions

--- Oracle/Clone-powershell/master-clone.ps1
+++ Oracle/Clone-powershell/master-clone.ps1
@@ -1,8 +1,187 @@
+#region sdt
+# Set SDT in LM so that On-call doesn't get alerted
+# 
+<# Use TLS 1.2 #>
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+<# account info #>
+$accessId = 'D889JMm2Xrbv5N3f4W2k'
+$accessKey = 'Ma9e3f6T_7-ErJa8^25CiXYvZL+An=}hmtdMtfzJ'
+$company = 'powerdesigninc'
+
+<# Varibles for setting SDT per device and time frame#>
+$sdtDuration = '3600000' <# time in milliseconds for the duration of SDT, 3600000 is 1H, 7200000 is 2H etc etc#>
+$esx23id = '351'
+$esx25id = '297'
+$vcsa02id = '1173'
+$vcsa04id = '1473'
+
+<# Get current time in milliseconds #>
+$epoch = [Math]::Round((New-TimeSpan -start (Get-Date -Date "1/1/1970") -end (Get-Date).ToUniversalTime()).TotalMilliseconds)
+$epochSDTstart = [Math]::Round((New-TimeSpan -start (Get-Date -Date "1/1/1970") -end (Get-Date).ToUniversalTime()).TotalMilliseconds)
+$epochSDTEnd = $epochSDTStart + $sdtDuration
+
+<# request details #>
+$httpVerb = 'POST'
+$resourcePath = '/sdt/sdts'
+$data = "{`"sdtType`":1,`"type`":`"DeviceSDT`",`"deviceId`":$esx23id,`"startDateTime`":$epochSDTstart,`"endDateTime`":$epochSDTend}"
+
+<# Construct URL #>
+$url = 'https://' + $company + '.logicmonitor.com/santaba/rest' + $resourcePath
+
+<# Concatenate Request Details #>
+$requestVars = $httpVerb + $epoch + $data + $resourcePath
+
+<# Construct Signature #>
+$hmac = New-Object System.Security.Cryptography.HMACSHA256
+$hmac.Key = [Text.Encoding]::UTF8.GetBytes($accessKey)
+$signatureBytes = $hmac.ComputeHash([Text.Encoding]::UTF8.GetBytes($requestVars))
+$signatureHex = [System.BitConverter]::ToString($signatureBytes) -replace '-'
+$signature = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($signatureHex.ToLower()))
+
+<# Construct Headers #>
+$auth = 'LMv1 ' + $accessId + ':' + $signature + ':' + $epoch
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Authorization", $auth)
+$headers.Add("Content-Type", 'application/json')
+
+<# Make Request #>
+$response = Invoke-RestMethod -Uri $url -Method $httpVerb -Body $data -Header $headers
+
+<# Print status and body of response1 #>
+$status = $response.status
+$body = $response.data | ConvertTo-Json -Depth 5
+
+Write-Host "Status:$status"
+Write-Host "Response:$body"
+
+<# Get current time in milliseconds #>
+$epoch = [Math]::Round((New-TimeSpan -start (Get-Date -Date "1/1/1970") -end (Get-Date).ToUniversalTime()).TotalMilliseconds)
+$epochSDTstart = [Math]::Round((New-TimeSpan -start (Get-Date -Date "1/1/1970") -end (Get-Date).ToUniversalTime()).TotalMilliseconds)
+$epochSDTEnd = $epochSDTStart + $sdtDuration
+
+<# request details #>
+$httpVerb = 'POST'
+$resourcePath = '/sdt/sdts'
+$data = "{`"sdtType`":1,`"type`":`"DeviceSDT`",`"deviceId`":$esx25id,`"startDateTime`":$epochSDTstart,`"endDateTime`":$epochSDTend}"
+
+<# Construct URL #>
+$url = 'https://' + $company + '.logicmonitor.com/santaba/rest' + $resourcePath
+
+<# Concatenate Request Details #>
+$requestVars = $httpVerb + $epoch + $data + $resourcePath
+
+<# Construct Signature #>
+$hmac = New-Object System.Security.Cryptography.HMACSHA256
+$hmac.Key = [Text.Encoding]::UTF8.GetBytes($accessKey)
+$signatureBytes = $hmac.ComputeHash([Text.Encoding]::UTF8.GetBytes($requestVars))
+$signatureHex = [System.BitConverter]::ToString($signatureBytes) -replace '-'
+$signature = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($signatureHex.ToLower()))
+
+<# Construct Headers #>
+$auth = 'LMv1 ' + $accessId + ':' + $signature + ':' + $epoch
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Authorization", $auth)
+$headers.Add("Content-Type", 'application/json')
+
+<# Make Request #>
+$response = Invoke-RestMethod -Uri $url -Method $httpVerb -Body $data -Header $headers
+
+<# Print status and body of response1 #>
+$status = $response.status
+$body = $response.data | ConvertTo-Json -Depth 5
+
+Write-Host "Status:$status"
+Write-Host "Response:$body"
+
+<# Get current time in milliseconds #>
+$epoch = [Math]::Round((New-TimeSpan -start (Get-Date -Date "1/1/1970") -end (Get-Date).ToUniversalTime()).TotalMilliseconds)
+$epochSDTstart = [Math]::Round((New-TimeSpan -start (Get-Date -Date "1/1/1970") -end (Get-Date).ToUniversalTime()).TotalMilliseconds)
+$epochSDTEnd = $epochSDTStart + $sdtDuration
+
+<# request details #>
+$httpVerb = 'POST'
+$resourcePath = '/sdt/sdts'
+$data = "{`"sdtType`":1,`"type`":`"DeviceSDT`",`"deviceId`":$vcsa02id,`"startDateTime`":$epochSDTstart,`"endDateTime`":$epochSDTend}"
+
+<# Construct URL #>
+$url = 'https://' + $company + '.logicmonitor.com/santaba/rest' + $resourcePath
+
+<# Concatenate Request Details #>
+$requestVars = $httpVerb + $epoch + $data + $resourcePath
+
+<# Construct Signature #>
+$hmac = New-Object System.Security.Cryptography.HMACSHA256
+$hmac.Key = [Text.Encoding]::UTF8.GetBytes($accessKey)
+$signatureBytes = $hmac.ComputeHash([Text.Encoding]::UTF8.GetBytes($requestVars))
+$signatureHex = [System.BitConverter]::ToString($signatureBytes) -replace '-'
+$signature = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($signatureHex.ToLower()))
+
+<# Construct Headers #>
+$auth = 'LMv1 ' + $accessId + ':' + $signature + ':' + $epoch
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Authorization", $auth)
+$headers.Add("Content-Type", 'application/json')
+
+<# Make Request #>
+$response = Invoke-RestMethod -Uri $url -Method $httpVerb -Body $data -Header $headers
+
+<# Print status and body of response1 #>
+$status = $response.status
+$body = $response.data | ConvertTo-Json -Depth 5
+
+Write-Host "Status:$status"
+Write-Host "Response:$body"
+
+
+<# Get current time in milliseconds #>
+$epoch = [Math]::Round((New-TimeSpan -start (Get-Date -Date "1/1/1970") -end (Get-Date).ToUniversalTime()).TotalMilliseconds)
+$epochSDTstart = [Math]::Round((New-TimeSpan -start (Get-Date -Date "1/1/1970") -end (Get-Date).ToUniversalTime()).TotalMilliseconds)
+$epochSDTEnd = $epochSDTStart + $sdtDuration
+
+<# request details #>
+$httpVerb = 'POST'
+$resourcePath = '/sdt/sdts'
+$data = "{`"sdtType`":1,`"type`":`"DeviceSDT`",`"deviceId`":$vcsa04id,`"startDateTime`":$epochSDTstart,`"endDateTime`":$epochSDTend}"
+
+<# Construct URL #>
+$url = 'https://' + $company + '.logicmonitor.com/santaba/rest' + $resourcePath
+
+<# Concatenate Request Details #>
+$requestVars = $httpVerb + $epoch + $data + $resourcePath
+
+<# Construct Signature #>
+$hmac = New-Object System.Security.Cryptography.HMACSHA256
+$hmac.Key = [Text.Encoding]::UTF8.GetBytes($accessKey)
+$signatureBytes = $hmac.ComputeHash([Text.Encoding]::UTF8.GetBytes($requestVars))
+$signatureHex = [System.BitConverter]::ToString($signatureBytes) -replace '-'
+$signature = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($signatureHex.ToLower()))
+
+<# Construct Headers #>
+$auth = 'LMv1 ' + $accessId + ':' + $signature + ':' + $epoch
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Authorization", $auth)
+$headers.Add("Content-Type", 'application/json')
+
+<# Make Request #>
+$response = Invoke-RestMethod -Uri $url -Method $httpVerb -Body $data -Header $headers
+
+<# Print status and body of response1 #>
+$status = $response.status
+$body = $response.data | ConvertTo-Json -Depth 5
+
+Write-Host "Status:$status"
+Write-Host "Response:$body"
+
+#endregion sdt
+#region input
 # Read in the users choice for the oracle clone instance value
 # 
 param (
     [string]$labn = $(Read-Host "Oracle Lab Instance Value (01,02,03,04,05)" )
     )
+#endregion input
+#region variables
 
 # Current as of PowerCLI 12 for Powershell 7 core
 # Declare the variables to use in the script
@@ -39,7 +218,9 @@ $ramAPP = '8'
 # 
 $vcpuDB = '2'
 $ramDB = '28'
+#endregion variables
 
+#region connections
 # Connect to the vCenter that you want to work with
 # 
 Write-Host "Connecting to the VCSA appliances we need to use" -ForegroundColor Red
@@ -51,7 +232,9 @@ Start-Sleep -s 2
 # 
 $esxcli25 = Get-EsxCli -VMhost pdi-esx25.powerdesigninc.us -V2
 $esxcli23 = Get-EsxCli -VMhost pdi-esx23.powerdesigninc.us -V2
+#endregion connections
 
+#region getdata
 # Get the Hosts in the vCenter so that we can use them later
 # 
 Write-Host "Get vSphere hosts in the cluster" -ForegroundColor Red
@@ -84,6 +267,10 @@ $vmappds = get-vm -Server $vcsa02 -name ("*APP" + $labn) | ForEach-Object { (Get
 $vmdbds = get-vm -Server $vcsa04 -name ("*DB" + $labn) | ForEach-Object { (Get-View $_.Id).datastore.value }
 Start-Sleep -s 1
 
+#endregion getdata
+
+#region vsphereactions
+
 # Stops the local VM from Running
 # 
 Write-Host "Stop the local VMs" -ForegroundColor Red
@@ -105,7 +292,11 @@ Remove-Datastore -Datastore ('LABAPP'+$labn) -VMHost 'pdi-esx25.powerdesigninc.u
 Remove-Datastore -Datastore ('LAB' + $labn + 'DB1'), ('LAB' + $labn + 'DB2'), ('LAB' + $labn + 'DB3'), ('LAB' + $labn + 'DB4'), ('LAB' + $labn + 'DB5'), ('LAB' + $labn + 'DB6'), ('LAB' + $labn + 'DB7') -VMHost 'pdi-esx23.powerdesigninc.us' -Confirm:$false
 Start-Sleep -s 1
 
-clear
+#endregion vsphereactions
+
+#region nimble
+
+Clear-Host
 Write-Host "About to do things with Nimble" -ForegroundColor Red
 
 # 
@@ -202,7 +393,7 @@ $action = Remove-NSVolume -id $responseid
 
 Write-Host "Finished with removing and deleting the Volumes - Break out if you do NOT want to create new volumes" -ForegroundColor Red -BackgroundColor Yellow
 pause
-clear
+Clear-Host
 
 # Start the clone process
 # Discover the last snap ID to be taken on the Oracle App volume
@@ -288,6 +479,10 @@ New-NSAccessControlRecord -initiator_group_id 025b62a468b66e547c0000000000000000
 # 
 Disconnect-NSGroup
 Write-Host "Disconnected from the Nimble array" -ForegroundColor Green
+
+#endregion nimble
+
+#region vsphere2
 
 # Refresh the iSCSI adapter, rescan it for new VMFS volumes, rescanning all HBAs
 # 
@@ -389,6 +584,8 @@ Start-Sleep -s 1
 
 # Reconfigure the DB vmx file because we added new disks
 # 
+
+#region vspherevdisk
 
 # Global variables
 #
@@ -536,6 +733,8 @@ $body = "{
 $response = Invoke-RestMethod "https://172.27.1.157/rest/vcenter/vm/$vmdbid/hardware/disk/2005" -SkipCertificateCheck -Method 'PATCH' -Headers $headers -Body $body
 $response | ConvertTo-Json
 
+#endregion vspherevdisk
+
 
 # Power the VM on and Answer the question of copying it
 # 
@@ -562,3 +761,4 @@ Start-VM $newAPPname
 Start-VM $newDBname
 
 Write-Host "We're done here - don't forget to remove your SDT" -ForegroundColor Green
+#endregion vsphere2
